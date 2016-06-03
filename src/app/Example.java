7a5a15928baade732c2519d8f0a19126da56cd8f
Александр Горбатько <avgorbatko@chel.rusoft.local>
2016-06-03 09:41:33 +0500

    init

---
+++ src/app/Example.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Example {
+
+	
+	/**
+	 * <p> Main method </p>
+	 * 
+	 * Выводит число, возвращаемое методом {@link #getInt() getInt}
+	 * 
+	 * @param args Параметры 
+	 */
+	public static void main(String[] args) {
+		String s = "fdgdfgdfg";
+		
+		int i = getInt();
+		
+		System.out.println(i);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * 
+	 * @return Число 4
+	 */
+	public static int getInt(){
+		return 4;
+	}
+	
+	public static int permutate(int i) {
+		return i+5;
+	}
+
+}
---
+++ src/app/ExampleTest.java
@@ -0,0 +1,35 @@
+package app;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+public class ExampleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void getIntShouldReturn5() {
+		assertThat(Example.getInt(), anyOf(is(5), is(4)) );
+	}
+
+}
